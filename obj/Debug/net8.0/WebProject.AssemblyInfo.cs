5dd4fc7014cc45abe3e699cac2cd1f8df5b4aa65
vg-42 <vgurnani42@gmail.com>
2024-07-30 10:34:26 +0530

    correcting paths in yaml file

--- obj/Debug/net8.0/WebProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d596d6bd0e5f0559aa3a78528d14e8fbb71bfa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
